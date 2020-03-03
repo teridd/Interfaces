b54dc05b14726183e13a926483ffed5f7f97cdf9
stramline <31758446+stramline@users.noreply.github.com>
2020-03-03 15:26:50 +0100

    Initial InterefaceTest

---
+++ src/InterfaceTest.java
@@ -0,0 +1,44 @@
+/*
+Learn Programming Academy's Java 1Z0-815 Certification Exam Course
+Section 10: Programming Abstractly Through Interfaces
+Topic: Create and implement interfaces
+*/
+
+interface InterfaceExample {
+
+    // Define some variables
+
+    // no modifiers at all
+    String interfaceName = "InterfaceExample";
+
+    // single modifer
+    static String staticInterfaceName = "StaticInterfaceExample";
+    public String publicInterfaceName = "PublicInterfaceName";
+    final String finalInterfaceName = "FinalInterfaceName";
+
+    // double modifiers
+    public final String publicFinalInterfaceName = "PublicFinalInterfaceName";
+    static final String staticFinalInterfaceName = "StaticFinalInterfaceName";
+
+    // triple modifiers
+    public static final String publicStaticFinalInterfaceName = "PublicStaticFinalInterfaceName";
+
+}
+
+public class InterfaceTest {
+    public static void main(String[] args) {
+
+        // Regardless of how you define it, a variable on an interface is
+        // public static final
+        System.out.println("All fields on an interface are" +
+                " public static final:");
+        System.out.println(InterfaceExample.interfaceName);
+        System.out.println(InterfaceExample.staticInterfaceName);
+        System.out.println(InterfaceExample.publicInterfaceName);
+        System.out.println(InterfaceExample.finalInterfaceName);
+        System.out.println(InterfaceExample.publicFinalInterfaceName);
+        System.out.println(InterfaceExample.staticFinalInterfaceName);
+        System.out.println(InterfaceExample.publicStaticFinalInterfaceName);
+
+    }
+}
